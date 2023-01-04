591225be0f497e7dca386a6010c100269ec3695c
lishengyu <lishengyu@baidu.com>
2023-01-04 20:56:20 +0800

    add vitepress

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,15 @@ import Button from './button';
 import SFCButton from './SFCButton.vue';
 
 createApp({
-  template: `
-  <div>
-    <SButton color="blue" round plain icon="search">蓝色按钮</SButton>
-    <SButton color="green">绿色按钮</SButton>
-    <SButton color="gray">灰色按钮</SButton>
-    <SButton color="yellow">黄色按钮</SButton>
-    <SButton color="red">红色按钮</SButton>
-  </div>
-  `,
+    template: `
+        <div>
+            <SButton color="blue" round icon="search">蓝色按钮</SButton>
+            <SButton color="green">绿色按钮</SButton>
+            <SButton color="gray">灰色按钮</SButton>
+            <SButton color="yellow">黄色按钮</SButton>
+            <SButton color="red">红色按钮</SButton>
+        </div>
+        `,
 })
-  .use(SmartyUI)
-  .mount('#app');
+    .use(SmartyUI)
+    .mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -6,35 +6,36 @@ import { presetUno, presetAttributify, presetIcons } from 'unocss';
 import Unocss from './config/unocss';
 
 const rollupOptions = {
-  external: ['vue', 'vue-router'],
-  output: {
-    globals: {
-      vue: 'Vue',
+    external: ['vue', 'vue-router'],
+    output: {
+        globals: {
+            vue: 'Vue',
+        },
     },
-  },
 };
 
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx({}),
-    // Unocss({
-    //   presets: [presetUno(), presetAttributify(), presetIcons()],
-    // }),
-    Unocss(),
-  ],
+    plugins: [
+        vue(),
+        vueJsx({}),
+        // Unocss({
+        //   presets: [presetUno(), presetAttributify(), presetIcons()],
+        // }),
+        Unocss(),
+    ],
 
-  build: {
-    rollupOptions,
-    minify: false,
-    lib: {
-      entry: './src/entry.ts',
-      name: 'SmartyUI',
-      fileName: 'smarty-ui',
-      // 导出模块格式
-      formats: ['esm', 'umd', 'iife'],
+    build: {
+        rollupOptions,
+        minify: false,
+        cssCodeSplit: true,
+        lib: {
+            entry: './src/entry.ts',
+            name: 'SmartyUI',
+            fileName: 'smarty-ui',
+            // 导出模块格式
+            formats: ['esm', 'umd', 'iife'],
+        },
     },
-  },
 });
